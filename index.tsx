2fd4e5fa323fc2374e39616c2362ba284f73324f
Julian Jelfs <julian.jelfs@ef.com>
2020-09-18 17:00:34 +0100

    that's why everything was huge

--- index.tsx
+++ index.tsx
@@ -2,8 +2,4 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { TableWrapper } from './table';
 
-const element = <h1>
-    <TableWrapper />
-</h1>
-
-ReactDOM.render(element, document.getElementById("app"));
+ReactDOM.render(<TableWrapper/>, document.getElementById("app"));
